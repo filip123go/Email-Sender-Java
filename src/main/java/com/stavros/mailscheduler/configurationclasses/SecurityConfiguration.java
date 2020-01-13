0ff696fc7c9468dd9e8e154b6be7e72008a9f465
Stavros Filippidis <stavros489@hotmail.com>
2020-01-13 23:01:35 +0200

    Spring security up

--- src/main/java/com/stavros/mailscheduler/configurationclasses/SecurityConfiguration.java
+++ src/main/java/com/stavros/mailscheduler/configurationclasses/SecurityConfiguration.java
@@ -3,19 +3,43 @@ package com.stavros.mailscheduler.configurationclasses;
 import javax.sql.DataSource;
 
 import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @AllArgsConstructor
+@EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-	private DataSource dataSource;
+    private DataSource dataSource;
 
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.jdbcAuthentication()
-				.dataSource(this.dataSource);
-	}
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//		auth.jdbcAuthentication()
+//				.dataSource(this.dataSource);
+
+        auth
+                .inMemoryAuthentication()
+                .withUser("admin").password(passwordEncoder().encode("admin")).roles("ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated() //authorize any request from authenticated users
+                .and()
+                .httpBasic();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 }
