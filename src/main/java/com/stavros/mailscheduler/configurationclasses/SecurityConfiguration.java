3fab3eb683ff5f1b63f92b77c3c9959dcc9d1fe2
filippidis <filippidis@intralot.com>
2020-01-14 12:36:54 +0200

    roles added

--- src/main/java/com/stavros/mailscheduler/configurationclasses/SecurityConfiguration.java
+++ src/main/java/com/stavros/mailscheduler/configurationclasses/SecurityConfiguration.java
@@ -17,29 +17,41 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-    private DataSource dataSource;
+	private DataSource dataSource;
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 //		auth.jdbcAuthentication()
 //				.dataSource(this.dataSource);
 
-        auth
-                .inMemoryAuthentication()
-                .withUser("admin").password(passwordEncoder().encode("admin")).roles("ADMIN");
-    }
+		auth
+				.inMemoryAuthentication()
+				.withUser("admin")
+				.password(passwordEncoder().encode("admin")).roles("ADMIN") //mandatory to use encoder
+				.and()
+				.withUser("stavros")
+				.password(passwordEncoder().encode("stavros")).roles("USER") //mandatory to use encoder
+				.and()
+				.withUser("manager")
+				.password(passwordEncoder().encode("manager")).roles("MANAGER"); //mandatory to use encoder
+	}
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .anyRequest().authenticated() //authorize any request from authenticated users
-                .and()
-                .httpBasic();
-    }
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+				.authorizeRequests()
+				.antMatchers("/index.html").permitAll() // the index.html is accessible by everyone
+				.antMatchers("/swagger-ui.html").authenticated() // the /swagger-ui.html can be accessed only by authenticated users (you must login)
+				.antMatchers("/admin/**").hasRole("ADMIN") //accessible only to "ADMINS"
+				.antMatchers("/management/**").hasAnyRole("ADMIN","MANAGER")//accessible only to "ADMINS" and "MANAGERS"
+//				.anyRequest().authenticated() //authorize any request from authenticated users (any request needs to be authenticated)
+				.and()
+				.httpBasic();
+		//we can use /** to select all the subdomains from a specific path ex: /management/**"
+	}
 
-    @Bean
-    PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
-    }
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 }
